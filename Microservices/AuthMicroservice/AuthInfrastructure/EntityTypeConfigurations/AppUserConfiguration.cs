01cf727e200983a4404868375ce0fb009747e19d
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2023-12-26 15:44:46 +0300

    add-configuration

---
+++ Microservices/AuthMicroservice/AuthInfrastructure/EntityTypeConfigurations/AppUserConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Auth.Infrastructure.EntityTypeConfigurations
+{
+    public class AppUserConfiguration : IEntityTypeConfiguration<IdentityUser>
+    {
+        public void Configure(EntityTypeBuilder<IdentityUser> builder)
+        {
+            builder.HasKey(x => x.Id);
+        }
+    }
+}
