aaf10aa8e8ac672f8e2b5e4796d8109debd60cf3
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2023-12-26 21:46:04 +0300

    add-app-user

---
+++ Microservices/AuthMicroservice/AuthCore/Entities/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Auth.Core.Entities
+{
+    public class AppUser : IdentityUser<long>
+    {
+        public string? RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiryTime { get; set; }
+    }
+}
