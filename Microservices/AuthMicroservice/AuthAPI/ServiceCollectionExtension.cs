a3b702056d5743165e9f48a8b089a3510db5739d
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2023-12-26 21:03:16 +0300

    add-dbconnection

---
+++ Microservices/AuthMicroservice/AuthAPI/ServiceCollectionExtension.cs
@@ -0,0 +1,30 @@
+﻿using Auth.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auth.API
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection ConfigureEndpointsApiExplorer(this IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+
+            return services;
+        }
+
+        public static IServiceCollection ConfigureSQLServer(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AuthDbContext>(options
+                 => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            return services;
+        }
+
+        public static IServiceCollection ConfigureControllers(this IServiceCollection services)
+        {
+            services.AddControllers();
+
+            return services;
+        }
+    }
+}
