e0538d94aea63f4f1ff80d92664944e593715a37
AnnaZalevskaya <123122231+AnnaZalevskaya@users.noreply.github.com>
2023-12-27 17:34:27 +0300

    Feature/add infrastructure for auth microservice (#2)
    * add-authdbcontext
    
    * add-configuration
    
    * add-dbconnection
    
    * add-app-user
    
    * edit-context
    
    * correction
    
    * changing-the-conn-str
    
    * changing-the-configuration
    
    * implementation-of-migrations
    
    * add-migration-manager
    
    * removing-unnecessary
    
    * removing-the-try-catch-block

---
+++ Microservices/AuthMicroservice/AuthInfrastructure/Data/AuthDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Auth.Core.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Auth.Infrastructure.Data
+{
+    public class AuthDbContext : IdentityDbContext<AppUser>
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options)
+            : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(builder);
+        }
+    }
+}
