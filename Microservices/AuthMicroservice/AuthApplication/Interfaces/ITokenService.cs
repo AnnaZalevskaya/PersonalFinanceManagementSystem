19caf07fbe82f9c3e06b2e5f1fe90c23b87af03f
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2023-12-27 23:58:08 +0300

    add-token-service

---
+++ Microservices/AuthMicroservice/AuthApplication/Interfaces/ITokenService.cs
@@ -0,0 +1,10 @@
+﻿using Auth.Core.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace Auth.Application.Interfaces
+{
+    public interface ITokenService
+    {
+        string CreateToken(AppUser user, List<IdentityRole> role);
+    }
+}
---
+++ Microservices/AuthMicroservice/AuthApplication/Services/TokenService.cs
@@ -0,0 +1,28 @@
+﻿using Auth.API.Extensions;
+using Auth.Core.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace Auth.Application.Services
+{
+    public class TokenService
+    {
+        private readonly IConfiguration _configuration;
+
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string CreateToken(AppUser user, List<IdentityRole> roles)
+        {
+            var token = user
+                .CreateClaims(roles)
+                .CreateJwtToken(_configuration);
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
