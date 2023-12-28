ab0b98de7a6bdaad6ba1b857226f374151f56b96
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2023-12-28 16:57:55 +0300

    add-mapp-config

---
+++ Microservices/AuthMicroservice/AuthApplication/MappConfigs/RegReqMappConfig.cs
@@ -0,0 +1,14 @@
+﻿using Auth.Application.Models;
+using Auth.Core.Entities;
+using AutoMapper;
+
+namespace Auth.Application.MappConfigs
+{
+    public class RegReqMappConfig : Profile
+    {
+        public RegReqMappConfig() 
+        {
+            CreateMap<RegisterRequest, AppUser>().ReverseMap();
+        }
+    }
+}
