dc97f41c2a7b83c6597b85c84fc713abf32e7fed
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2024-01-04 23:54:22 +0300

    adding-startup-configuration

---
+++ Microservices/AccountsMicroservice/AccountsPL/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,81 @@
+﻿using Accounts.BusinessLogic.Services.Implementations;
+using Accounts.BusinessLogic.Services.Interfaces;
+using Accounts.DataAccess.Data;
+using Accounts.DataAccess.UnitOfWork;
+using FluentValidation.AspNetCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+namespace Accounts.Presentation.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection ConfigureEndpointsApiExplorer(this IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+
+            return services;
+        }
+
+        public static IServiceCollection ConfigureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Financial accounts API"
+                });
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection ConfigurePostgreSQL(this IServiceCollection services, 
+            IConfiguration configuration)
+        {
+            services.AddDbContext<FinancialAccountsDbContext>(options
+                 => options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+            return services;
+        }
+
+        public static IServiceCollection ConfigureRepositoryWrapper(this IServiceCollection services)
+        {
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+
+        public static IServiceCollection ConfigureAppServices(this IServiceCollection services)
+        {
+            services.AddScoped<IFinancialAccountService, FinancialAccountService>();
+            services.AddScoped<IFinancialAccountTypeService, FinancialAccountTypeService>();
+            services.AddScoped<ICurrencyService, CurrencyService>();
+
+            return services;
+        }
+
+        public static IServiceCollection ConfigureControllers(this IServiceCollection services)
+        {
+            services.AddControllers();
+
+            return services;
+        }
+
+        public static IServiceCollection ConfigureValidation(this IServiceCollection services)
+        {
+            services.AddFluentValidationAutoValidation()
+                .AddFluentValidationClientsideAdapters();
+
+            return services;
+        }
+
+        public static IServiceCollection ConfigureMapperProfiles(this IServiceCollection services)
+        {
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            return services;
+        }
+    }
+}
--- Microservices/AccountsMicroservice/AccountsPL/Program.cs
+++ Microservices/AccountsMicroservice/AccountsPL/Program.cs
@@ -1,25 +1,39 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿using Accounts.Presentation.Extensions;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace Accounts.Presentation
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services
+                .ConfigurePostgreSQL(builder.Configuration)
+                .ConfigureSwagger()
+                .ConfigureControllers()
+                .ConfigureValidation()
+                .ConfigureEndpointsApiExplorer()
+                .ConfigureRepositoryWrapper()
+                .ConfigureAppServices()
+                .ConfigureMapperProfiles();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
