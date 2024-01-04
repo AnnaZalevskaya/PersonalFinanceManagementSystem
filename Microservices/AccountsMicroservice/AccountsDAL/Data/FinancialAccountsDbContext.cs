620e73b3eb3fceae406f5a603729cd2fea8b2b82
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2024-01-04 23:48:20 +0300

    adding-db-and-configuration

---
+++ Microservices/AccountsMicroservice/AccountsDAL/Data/FinancialAccountsDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Accounts.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Accounts.DataAccess.Data
+{
+    public class FinancialAccountsDbContext : DbContext
+    {
+        public DbSet<FinancialAccount> FinancialAccounts { get; set; }
+        public DbSet<FinancialAccountType> FinancialAccountTypes { get; set; }
+        public DbSet<Currency> Currencies { get; set; }
+
+        public FinancialAccountsDbContext(DbContextOptions<FinancialAccountsDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(builder);
+        }
+    }
+}
