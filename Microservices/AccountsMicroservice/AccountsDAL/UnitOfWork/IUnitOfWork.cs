d6250dbb38b46a938826d72b21dc166b779f4d5f
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2024-01-04 23:51:28 +0300

    adding-repositories

---
+++ Microservices/AccountsMicroservice/AccountsDAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using Accounts.DataAccess.Repositories.Interfaces;
+
+namespace Accounts.DataAccess.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        IFinancialAccountRepository FinancialAccounts { get; }
+        IFinancialAccountTypeRepository FinancialAccountTypes { get; }
+        ICurrencyRepository Currencies { get; }
+
+        Task SaveChangesAsync();
+    }
+}
