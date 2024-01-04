20a94e380e17b50e055cf4bf83954f916e81dd58
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2024-01-04 23:47:03 +0300

    adding-entities

---
+++ Microservices/AccountsMicroservice/AccountsDAL/Entities/FinancialAccount.cs
@@ -0,0 +1,13 @@
+﻿namespace Accounts.DataAccess.Entities
+{
+    public class FinancialAccount
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int AccountTypeId { get; set; }
+        public FinancialAccountType? AccountType { get; set; }
+        public int CurrencyId { get; set; }
+        public Currency? Currency { get; set; }
+        public int UserId { get; set; }
+    }
+}
