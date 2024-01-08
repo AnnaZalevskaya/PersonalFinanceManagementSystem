8f820af0f769de2b8016e3b47b7a4f94119f5694
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2024-01-08 17:12:37 +0300

    add-db-connection

---
+++ Microservices/OperationsMicroservice/OperationsInfrastructure/Data/OperationsDbContext.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using Operations.Core.Entities;
+using Operations.Infrastructure.Settings;
+
+namespace Operations.Infrastructure.Data
+{
+    public class OperationsDbContext
+    {
+        private readonly MongoClient _client;
+        private readonly IMongoDatabase _database;
+        private readonly IMongoCollection<Category> _categories;
+        private readonly IMongoCollection<CategoryType> _categoryTypes;
+        private readonly IMongoCollection<Operation> _operations;
+
+        public OperationsDbContext(IOptions<DatabaseSettings> dbOptions)
+        {
+            var dbSettings = dbOptions.Value;
+            _client = new MongoClient(dbSettings.ConnectionString);
+            _database = _client.GetDatabase(dbSettings.DatabaseName);
+            _categories = _database.GetCollection<Category>(dbSettings.CategoriesCollectionName);
+            _categoryTypes = _database.GetCollection<CategoryType>(dbSettings.CategoryTypesCollectionName);
+            _operations = _database.GetCollection<Operation>(dbSettings.OperationsCollectionName);
+        }
+
+        public IMongoClient Client
+        {
+            get
+            {
+                return _client;
+            }
+        }
+
+        public IMongoDatabase Database
+        {
+            get
+            {
+                return _database;
+            }
+        }
+
+        public IMongoCollection<Category> Categories
+        {
+            get
+            {
+                return _categories;
+            }
+        }
+
+        public IMongoCollection<CategoryType> CategoryTypes
+        {
+            get
+            {
+                return _categoryTypes;
+            }
+        }
+
+        public IMongoCollection<Operation> Operations
+        {
+            get
+            {
+                return _operations;
+            }
+        }
+    }
+}
---
+++ Microservices/OperationsMicroservice/OperationsInfrastructure/Settings/DatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Operations.Infrastructure.Settings
+{
+    public class DatabaseSettings
+    {
+        public string CategoriesCollectionName { get; init; }
+        public string CategoryTypesCollectionName { get; init; }
+        public string OperationsCollectionName { get; init; }
+        public string ConnectionString { get; init; }
+        public string DatabaseName { get; init; }
+    }
+}
