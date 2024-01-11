69ecccb0b70e3517f923ca4db239294341c2eba3
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2024-01-11 13:22:20 +0300

    adding-controllers

---
+++ Microservices/OperationsMicroservice/OperationsAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Operations.Application.Models;
+using Operations.Application.Operations.Queries.GetCategoryDetails;
+using Operations.Application.Operations.Queries.GetCategoryList;
+using Operations.Application.Settings;
+
+namespace Operations.API.Controllers
+{
+    [ApiController]
+    [Route("api/categories")]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public CategoriesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAllAsync([FromQuery] PaginationSettings paginationSettings)
+        {
+            var categories = await _mediator.Send(new GetCategoryListQuery(paginationSettings));
+
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CategoryModel>> GetByIdAsync(int id)
+        {
+            var category = await _mediator.Send(new GetCategoryDetailsQuery(id));
+
+            return Ok(category);
+        }
+    }
+}
---
+++ Microservices/OperationsMicroservice/OperationsAPI/Controllers/CategoryTypesController.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Operations.Application.Models;
+using Operations.Application.Operations.Queries.GetCategoryTypeDetails;
+using Operations.Application.Operations.Queries.GetCategoryTypeList;
+using Operations.Application.Settings;
+
+namespace Operations.API.Controllers
+{
+    [ApiController]
+    [Route("api/category-types")]
+    public class CategoryTypesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public CategoryTypesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAllAsync([FromQuery] PaginationSettings paginationSettings)
+        {
+            var types = await _mediator.Send(new GetCategoryTypeListQuery(paginationSettings));
+
+            return Ok(types);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CategoryTypeModel>> GetByIdAsync(int id)
+        {
+            var type = await _mediator.Send(new GetCategoryTypeDetailsQuery(id));
+
+            return Ok(type);
+        }
+    }
+}
