81ec53cda74bba8b5edcb6ef230b46a9c003a179
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2024-01-11 13:23:34 +0300

    startup-configuration-and-adding-links

--- Microservices/OperationsMicroservice/OperationsAPI/Program.cs
+++ Microservices/OperationsMicroservice/OperationsAPI/Program.cs
@@ -1,25 +1,38 @@
-var builder = WebApplication.CreateBuilder(args);
+using Operations.API.Extensions;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace Operations.Api
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services
+                .ConfigureMongoDB(builder.Configuration)
+                .ConfigureSwagger()
+                .ConfigureControllers()
+                .ConfigureValidation()
+                .ConfigureEndpointsApiExplorer()
+                .ConfigureRepositoryWrapper()
+                .ConfigureMediatR()
+                .ConfigureMapperProfiles();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
