738a308540c9e43c804d6f47d8994106a4deecd7
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2024-01-12 23:05:57 +0300

    simplifying-model-transfer

--- Microservices/OperationsMicroservice/OperationsApplication/Operations/Commands/CreateOperation/CreateOperationCommand.cs
+++ Microservices/OperationsMicroservice/OperationsApplication/Operations/Commands/CreateOperation/CreateOperationCommand.cs
@@ -5,13 +5,11 @@ namespace Operations.Application.Operations.Commands.CreateOperation
 {
     public class CreateOperationCommand : IRequest
     {
-        public int AccountId { get; set; }
-        public Dictionary<string, object> Description { get; set; }
+        public CreateOperationModel Model { get; set; }
 
         public CreateOperationCommand(CreateOperationModel model)
         {
-            AccountId = model.AccountId;
-            Description = model.Description;
+            Model = model;
         }
     }
 }
