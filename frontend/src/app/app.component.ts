4764a89d4728ad53cf98c728d6fd2b21069cceac
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2024-02-28 22:23:38 +0300

    create-a-skeleton-of-page-structure

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { HttpClientModule } from '@angular/common/http';
+import { FinancialAccountsService } from './services/financial-accounts.service';
+import { AuthService } from './services/auth.service';
+import { OperationsService } from './services/operations.service';
+import { AccountTypesService } from './services/account-types.service';
+import { CurrencyService } from './services/currency.service';
+import { TokenService } from './services/token.service';
+import { UsersService } from './services/users.service';
+import { OperationCategoriesService } from './services/operation-categories.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    HeaderComponent, 
+    HttpClientModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css', 
+  providers: [
+    AuthService,
+    TokenService,
+    UsersService,
+    FinancialAccountsService,
+    AccountTypesService,
+    CurrencyService,
+    OperationsService, 
+    OperationCategoriesService
+  ]
 })
 export class AppComponent {
   title = 'frontend';
