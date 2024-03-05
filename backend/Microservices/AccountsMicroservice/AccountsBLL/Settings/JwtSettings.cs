a4bc06e4e271a28179f8ac68c7f37ae2e5201e4e
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2024-03-05 11:15:49 +0300

    making-edits

---
+++ backend/Microservices/AccountsMicroservice/AccountsBLL/Settings/JwtSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Accounts.BusinessLogic.Settings
+{
+    public class JwtSettings
+    {
+        public string Expire { get; init; }
+        public string Secret { get; init; }
+        public string Issuer { get; init; }
+        public string Audience { get; init; }
+        public string RefreshTokenValidityInDays { get; init; }
+    }
+}
