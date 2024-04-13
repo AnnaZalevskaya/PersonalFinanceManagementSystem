e8f08ce8efe00046cff6dd84b0bacb2b90bb7848
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2024-04-14 01:14:32 +0300

    adding-package

--- frontend/src/app/services/signal-r.service.ts
+++ frontend/src/app/services/signal-r.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import * as signalR from '@aspnet/signalr';
+import { HubConnectionBuilder, HubConnection, HubConnectionState } from '@microsoft/signalr';
 import { UserNotification } from '../models/user-notification.model';
 import { UserNotificationsService } from './user-notifications.service';
 
@@ -7,40 +7,44 @@ import { UserNotificationsService } from './user-notifications.service';
   providedIn: 'root'
 })
 export class SignalRService {
-  private hubConnection!: signalR.HubConnection;
+  private hubConnection!: HubConnection;
 
   constructor(private notificationsService: UserNotificationsService) {}
 
-  startConnection = () => {
-    this.hubConnection = new signalR.HubConnectionBuilder()
-      .withUrl('https://localhost:44304/notifications', {
-        skipNegotiation: true,
-        transport: signalR.HttpTransportType.WebSockets
-      }) 
+  startConnection(): void {
+    this.hubConnection = new HubConnectionBuilder()
+      .withUrl('https://localhost:44304/notifications')
       .build();
 
     this.hubConnection
       .start()
       .then(() => console.log('Connection started'))
-      .catch(err => console.log('Error while starting connection: ' + err));
+      .catch(err => console.error('Error while starting connection: ' + err));
   }
 
-  startNotificationsListener() {
+  startNotificationsListener(): void {
     this.hubConnection.on('ReceiveNotification', message => {
       const notification: UserNotification = { message: message, isRead: false };
       this.notificationsService.addNotification(notification);
     });
   }
 
-  sendNotification(userId: string, message: string) {
-    this.hubConnection.invoke("SendNotificationToUser", userId, message)
-      .catch(error => console.log("Error while sending message: " + error));
+  sendNotification(userId: string, message: string): void {
+    if (this.hubConnection && this.hubConnection.state === HubConnectionState.Connected) {
+      this.hubConnection.invoke("ReceiveNotification", userId, message)
+        .catch(error => console.error("Error while sending message: " + error));
+    } else {
+      console.error("Hub connection is not established.");
+    }
   }
 
-  stopConnection() {
-    this.hubConnection.stop().then(() => {
-      console.log("Hub connection stopped")
-    })
-      .catch(error => console.log("Error while stopping hub connection: " + error))
+  stopConnection(): void {
+    if (this.hubConnection && this.hubConnection.state === HubConnectionState.Connected) {
+      this.hubConnection.stop()
+        .then(() => console.log("Hub connection stopped"))
+        .catch(error => console.error("Error while stopping hub connection: " + error));
+    } else {
+      console.error("Hub connection is not established.");
+    }
   }
 }
