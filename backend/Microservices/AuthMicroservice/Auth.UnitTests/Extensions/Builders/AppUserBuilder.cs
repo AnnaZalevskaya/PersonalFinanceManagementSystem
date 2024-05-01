e9a6b023491a558d55c13447f57b282793f5d4d8
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2024-05-02 00:46:14 +0300

    adding-tests

---
+++ backend/Microservices/AuthMicroservice/Auth.UnitTests/Extensions/Builders/AppUserBuilder.cs
@@ -0,0 +1,14 @@
+﻿namespace Auth.UnitTests.Extensions.Builders
+{
+    public static class AppUserBuilder
+    {
+        public static AppUser BuildAppUser(int id, string email)
+        {
+            return new AppUser
+            {
+                Id = id,
+                Email = email
+            };
+        }
+    }
+}
---
+++ backend/Microservices/AuthMicroservice/Auth.UnitTests/Extensions/Builders/PaginationSettingsBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Auth.Application.Settings;
+
+namespace Auth.UnitTests.Extensions.Builders
+{
+    public static class PaginationSettingsBuilder
+    {
+        public static PaginationSettings BuildPaginationSettings(int pageNumber, int pageSize)
+        {
+            return new PaginationSettings
+            {
+                PageNumber = pageNumber,
+                PageSize = pageSize
+            };
+        }
+    }
+}
