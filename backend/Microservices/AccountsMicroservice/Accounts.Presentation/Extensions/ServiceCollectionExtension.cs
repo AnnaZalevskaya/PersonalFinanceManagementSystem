3f29820f9b85d8c986fe2690462fd9258381f53a
AnnaZalevskaya <123122231+AnnaZalevskaya@users.noreply.github.com>
2024-05-02 15:36:58 +0300

    Working with settings (#14)
    * Fix
    
    * changing-jwt-settings
    
    * configuring-nugget-packages
    
    * adding-new-settings

--- backend/Microservices/AccountsMicroservice/Accounts.Presentation/Extensions/ServiceCollectionExtension.cs
+++ backend/Microservices/AccountsMicroservice/Accounts.Presentation/Extensions/ServiceCollectionExtension.cs
@@ -90,7 +90,7 @@ namespace Accounts.Presentation.Extensions
                         ValidIssuer = jwtOptions.Value.Issuer,
                         ValidAudience = jwtOptions.Value.Audience,
                         IssuerSigningKey =
-                            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.Value.Secret))
+                            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.Value.SecretKey))
                     };
                 });
 
@@ -118,8 +118,11 @@ namespace Accounts.Presentation.Extensions
         public static IServiceCollection ConfigurePostgreSQL(this IServiceCollection services, 
             IConfiguration configuration)
         {
+            services.Configure<ConnectionStrings>(configuration.GetSection(nameof(ConnectionStrings)));
+            var connStrings = services.BuildServiceProvider().GetRequiredService<IOptions<ConnectionStrings>>();
+
             services.AddDbContext<FinancialAccountsDbContext>(options
-                 => options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+                 => options.UseNpgsql(connStrings.Value.DefaultConnection));
 
             return services;
         }
@@ -219,8 +222,10 @@ namespace Accounts.Presentation.Extensions
         public static IServiceCollection ConfigureHangfire(this IServiceCollection services, 
             IConfiguration configuration)
         {
+            var connStrings = services.BuildServiceProvider().GetRequiredService<IOptions<ConnectionStrings>>();
+
             services.AddHangfire(config => 
-                config.UsePostgreSqlStorage(configuration.GetConnectionString("HangfireConnection")));
+                config.UsePostgreSqlStorage(connStrings.Value.HangfireConnection));
             services.AddHangfireServer();
 
             return services;
