2ee0b6e042929ceb10ee4a59802973146094b053
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2024-05-15 14:04:01 +0300

    refactoring-pages-with-account-working

--- frontend/src/app/components/account/account.component.ts
+++ frontend/src/app/components/account/account.component.ts
@@ -8,6 +8,7 @@ import { OperationsService } from '../../services/operations.service';
 import { HttpClientModule } from '@angular/common/http';
 import { LoadingIndicatorComponent } from '../loading-indicator/loading-indicator.component';
 import { FormsModule } from '@angular/forms';
+import { PaginationSettings } from '../../settings/pagination-settings';
 
 @Component({
   selector: 'app-account',
@@ -25,37 +26,64 @@ import { FormsModule } from '@angular/forms';
 })
 export class AccountComponent implements OnInit {
   isLoadingForm: boolean = false;
+
   account!: Account;
   id!: string;
+  
+  paginationSettings: PaginationSettings = new PaginationSettings();
 
-  constructor(private route: ActivatedRoute, 
+  constructor(
+    private route: ActivatedRoute, 
     private accountService: FinancialAccountsService,
-    private operationsService: OperationsService) { }
+    private operationsService: OperationsService
+  ) { }
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
+
     if (id !== null) {
-      this.id = id;
-      this.accountService.getAccountById(id).subscribe(
-        account => {
-          this.account = account; 
-          this.account.operations = [];
-          this.operationsService.getOperationsByAccount(account.id.toString()).subscribe(
-            operations =>{
-              this.account.operations = operations;
-            }
-          )
-        },
-        error => {
-          console.error('Error retrieving accounts:', error);
-        }
-      );
-      }
-     else {
+      this.loadAccount(id);
+    }
+    else {
       (error: any) => {
         console.error('Error during getting id:', error);
-      };      
+      };  
     }
+
     this.isLoadingForm = true;
+  }
+
+  loadAccount(id: string) {
+    this.id = id;
+    this.accountService.getAccountById(id).subscribe(
+      account => {
+        this.account = account; 
+        this.account.operations = [];
+        this.loadOperations(account.id.toString());
+      },
+      error => {
+        console.error('Error retrieving accounts:', error);
+      }
+    );
+  }
+
+  loadOperations(id: string) {
+    this.operationsService.getOperationsByAccount(id, this.paginationSettings).subscribe(
+      operations =>{
+        this.account.operations = operations;
+      }
+    )
+  }
+
+  goToPreviousPage(): void {
+    if (this.paginationSettings.pageNumber > 1) {
+      this.paginationSettings.pageNumber--;
+      this.loadOperations(this.id);
+    }
+  }
+
+  goToNextPage(): void {
+    this.paginationSettings.pageNumber++;
+    this.loadOperations(this.id);
   }
 }
--- frontend/src/app/components/all-accounts/all-accounts.component.ts
+++ frontend/src/app/components/all-accounts/all-accounts.component.ts
@@ -3,28 +3,35 @@ import { FinancialAccountsService } from '../../services/financial-accounts.serv
 import { Account } from '../../models/account.model';
 import { CommonModule } from '@angular/common';
 import { NgxPaginationModule } from 'ngx-pagination';
+import { PaginationSettings } from '../../settings/pagination-settings';
+import { LoadingIndicatorComponent } from '../loading-indicator/loading-indicator.component';
 
 @Component({
   selector: 'app-all-accounts',
   standalone: true,
   imports: [
     CommonModule,
-    NgxPaginationModule
+    NgxPaginationModule,
+    LoadingIndicatorComponent
   ],
   templateUrl: './all-accounts.component.html',
   styleUrl: './all-accounts.component.css'
 })
 export class AllAccountsComponent implements OnInit {
+  isLoadingForm: boolean = false;
+
   accounts: Account[] = [];
+  paginationSettings: PaginationSettings = new PaginationSettings();
 
   constructor(private accountService: FinancialAccountsService) { }
 
   ngOnInit(): void {
     this.loadAccounts();
+    this.isLoadingForm = true;
   }
 
   loadAccounts() {
-    this.accountService.getAccounts().subscribe(
+    this.accountService.getAccounts(this.paginationSettings).subscribe(
       accounts => {
         this.accounts = accounts;
       },
@@ -32,5 +39,17 @@ export class AllAccountsComponent implements OnInit {
         console.error('Error retrieving accounts:', error);
       }
     );
-  }  
+  } 
+  
+  goToPreviousPage(): void {
+    if (this.paginationSettings.pageNumber > 1) {
+      this.paginationSettings.pageNumber--;
+      this.loadAccounts();
+    }
+  }
+
+  goToNextPage(): void {
+    this.paginationSettings.pageNumber++;
+    this.loadAccounts();
+  }
 }
