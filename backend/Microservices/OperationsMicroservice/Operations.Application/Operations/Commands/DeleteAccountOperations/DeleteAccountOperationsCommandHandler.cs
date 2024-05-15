3ac32a28cfe1d604457610d6227df8d0a9308865
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2024-05-15 13:46:28 +0300

    delete-operations-by-account

---
+++ backend/Microservices/OperationsMicroservice/Operations.Application/Operations/Commands/DeleteAccountOperations/DeleteAccountOperationsCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Operations.Application.Interfaces;
+using Operations.Core.Exceptions;
+
+namespace Operations.Application.Operations.Commands.DeleteAccountOperations
+{
+    public class DeleteAccountOperationsCommandHandler : IRequestHandler<DeleteAccountOperationsCommand>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteAccountOperationsCommandHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task Handle(DeleteAccountOperationsCommand command, CancellationToken cancellationToken)
+        {
+            var operations = await _unitOfWork.Operations
+                .GetByAccountIdAsync(command.AccountId,  command.PaginationSettings, cancellationToken);
+
+            if (operations == null)
+            {
+                throw new EntityNotFoundException();
+            }
+
+            await _unitOfWork.Operations.DeleteByAccountIdAsync(command.AccountId, cancellationToken);
+        }
+    }
+}
