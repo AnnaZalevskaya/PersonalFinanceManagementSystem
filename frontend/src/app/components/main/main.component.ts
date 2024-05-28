1987c714e090cea96ceb9b5277c9fbf79723dc38
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2024-05-29 00:55:52 +0300

    modifying-main-page

--- frontend/src/app/components/main/main.component.ts
+++ frontend/src/app/components/main/main.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { ArticleCardComponent } from '../article-card/article-card.component';
+import { FooterComponent } from "../footer/footer.component";
+import { TemplatesCarouselComponent } from "../templates-carousel/templates-carousel.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './main.component.html',
+    styleUrl: './main.component.css',
+    imports: [
+      ArticleCardComponent,
+      FooterComponent,
+      TemplatesCarouselComponent
+    ]
 })
 export class MainComponent {
 
