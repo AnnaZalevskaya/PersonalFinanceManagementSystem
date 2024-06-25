3ecf4675ad26fc7c6b0ab0f7267116edfb8507f5
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2024-06-25 12:16:43 +0300

    pdf-report-on-account-side

--- backend/Microservices/AccountsMicroservice/Accounts.BusinessLogic/Services/Implementations/AccountPdfReportService.cs
+++ backend/Microservices/AccountsMicroservice/Accounts.BusinessLogic/Services/Implementations/AccountPdfReportService.cs
@@ -1,45 +1,14 @@
-﻿using Accounts.BusinessLogic.Models;
+﻿using Accounts.BusinessLogic.Extensions;
+using Accounts.BusinessLogic.Models;
 using Accounts.BusinessLogic.Services.Interfaces;
 using iText.Kernel.Pdf;
-using iText.Kernel.Pdf.Canvas.Draw;
 using iText.Layout;
 using iText.Layout.Element;
-using iText.Layout.Properties;
 
 namespace Accounts.BusinessLogic.Services.Implementations
 {
     public class AccountPdfReportService : IAccountPdfReportService
     {
-        public void GeneratePdfReportFromModel(FinancialAccountModel model)
-        {
-            string downloadsPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Downloads";
-            string outputPath = Path.Combine(downloadsPath, $"FinancialAccount_{model.Id}.pdf");
-
-            using (var writer = new PdfWriter(outputPath))
-            {
-                using (var pdfDocument = new PdfDocument(writer))
-                {
-                    var document = new Document(pdfDocument);
-
-                    document
-                        .SetFontSize(24);
-
-                    var header = new Paragraph($"Financial account num.{model.Id}:")
-                        .SetTextAlignment(TextAlignment.CENTER)
-                        .SetFontSize(20);
-                    document.Add(header);
-                    var ls = new LineSeparator(new SolidLine());
-                    document.Add(ls);
-                    document.Add(new Paragraph($"Name: {model.Name}"));
-                    document.Add(new Paragraph($"Type of financial account: {model.AccountType.Name}"));
-                    document.Add(new Paragraph($"Balance: {model.Balance} {model.Currency.Abbreviation}"));
-                    document.Add(new Paragraph($"Currency: {model.Currency.Name}"));
-
-                    document.Close();
-                }
-            }
-        }
-
         public async Task<byte[]> GeneratePdfReportFromAccountModel(FinancialAccountModel model)
         {
             var memoryStream = new MemoryStream();
@@ -50,11 +19,7 @@ namespace Accounts.BusinessLogic.Services.Implementations
                 {
                     var document = new Document(pdfDocument);
 
-                    var header = new Paragraph("Financial account:");
-                    document.Add(header);
-                    document.Add(new Paragraph($"Name: {model?.Name}"));
-                    document.Add(new Paragraph($"Type of financial account: {model?.AccountType.Name}"));
-                    document.Add(new Paragraph($"Balance: {model?.Balance}"));
+                    document.ApplyDocContentAndStyle(model);
 
                     document.Close();
                 }
---
+++ backend/Microservices/AccountsMicroservice/Accounts.Presentation/Controllers/PdfReportsController.cs
@@ -0,0 +1,29 @@
+﻿using Accounts.BusinessLogic.Models;
+using Accounts.BusinessLogic.Models.Consts;
+using Accounts.BusinessLogic.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Accounts.Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/pdf-account-report")]
+    public class PdfReportController : ControllerBase
+    {
+        private readonly IAccountPdfReportService _pdfReportService;
+
+        public PdfReportController(IAccountPdfReportService pdfReportService)
+        {
+            _pdfReportService = pdfReportService;
+        }
+
+        [HttpPost("report/{accountId}")]
+        [Authorize(Policy = AuthPolicyConsts.ClientOnly)]
+        public async Task<ActionResult> GenerateReportAsync(int accountId, [FromBody] FinancialAccountModel model)
+        {
+            var file = await _pdfReportService.GeneratePdfReportFromAccountModel(model);
+
+            return Ok(file);
+        }
+    }
+}
