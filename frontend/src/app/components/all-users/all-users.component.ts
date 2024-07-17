b5f0de1e7d860e3b97e6cc213335e7484d12c1f7
AnnaZalevskaya <123122231+AnnaZalevskaya@users.noreply.github.com>
2024-07-17 20:32:23 +0300

    Update all-users.component.ts

--- frontend/src/app/components/all-users/all-users.component.ts
+++ frontend/src/app/components/all-users/all-users.component.ts
@@ -1,16 +1,27 @@
 import { Component } from '@angular/core';
-import { User } from '../../models/user.model';
-import { UsersService } from '../../services/users.service';
+import { User } from '../../../models/user.model';
+import { UsersService } from '../../../services/users.service';
 import { CommonModule } from '@angular/common';
-import { PaginationSettings } from '../../settings/pagination-settings';
-import { LoadingIndicatorComponent } from '../loading-indicator/loading-indicator.component';
+import { PaginationSettings } from '../../../settings/pagination-settings';
+import { LoadingIndicatorComponent } from '../../additional-pages/loading-indicator/loading-indicator.component';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-all-users',
   standalone: true,
   imports: [
     CommonModule,
-    LoadingIndicatorComponent
+    LoadingIndicatorComponent,
+    MatCardModule,
+    MatPaginatorModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule
   ],
   templateUrl: './all-users.component.html',
   styleUrl: './all-users.component.css'
@@ -20,10 +31,20 @@ export class AllUsersComponent {
 
   users: User[] = [];
   paginationSettings: PaginationSettings = new PaginationSettings();
+  recordCount: number = 0;
 
-  constructor(private usersService: UsersService) { }
+  constructor(private usersService: UsersService, private router: Router) { }
 
   ngOnInit() {
+    this.usersService.getRecordsCount()
+      .subscribe(count => {
+        this.recordCount = count;
+      }
+    );
+
+    this.paginationSettings.pageSize = 3;
+    this.paginationSettings.pageNumber = 1;
+
     this.loadUsers();
     this.isLoadingForm = true;
   }
@@ -39,15 +60,15 @@ export class AllUsersComponent {
     );
   }  
 
-  goToPreviousPage(): void {
-    if (this.paginationSettings.pageNumber > 1) {
-      this.paginationSettings.pageNumber--;
-      this.loadUsers();
-    }
+  pageEvent!: PageEvent;
+
+  handlePageEvent(e: PageEvent) {
+    this.pageEvent = e;
+    this.paginationSettings.pageNumber = e.pageIndex + 1;
+    this.loadUsers();
   }
 
-  goToNextPage(): void {
-    this.paginationSettings.pageNumber++;
-    this.loadUsers();
+  back() {
+    this.router.navigate(['./admin']);
   }
 }
