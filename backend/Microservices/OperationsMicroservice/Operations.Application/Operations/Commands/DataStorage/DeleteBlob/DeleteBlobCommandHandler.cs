acaabd5eb952da31ee51d758e675608aaa468e75
AnnaZalevskaya <123122231+AnnaZalevskaya@users.noreply.github.com>
2024-07-25 11:39:06 +0300

    adding-blob-methods

---
+++ backend/Microservices/OperationsMicroservice/Operations.Application/Operations/Commands/DataStorage/DeleteBlob/DeleteBlobCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Azure.Storage.Blobs;
+using MediatR;
+using Microsoft.Extensions.Options;
+using Operations.Application.Settings;
+
+namespace Operations.Application.Operations.Commands.DataStorage.DeleteBlob
+{
+    public class DeleteBlobCommandHandler : IRequestHandler<DeleteBlobCommand>
+    {
+        private readonly AzureBlobStorageSettings _blobOptions;
+        private readonly BlobServiceClient _blobServiceClient;
+        private readonly BlobContainerClient _containerClient;
+
+        public DeleteBlobCommandHandler(BlobServiceClient blobServiceClient, IOptions<AzureBlobStorageSettings> blobOptions)
+        {
+            _blobOptions = blobOptions.Value;
+            _blobServiceClient = blobServiceClient;
+            _containerClient = _blobServiceClient.GetBlobContainerClient(_blobOptions.ContainerName);
+            _containerClient.CreateIfNotExists();
+        }
+
+        public async Task Handle(DeleteBlobCommand command, CancellationToken cancellationToken)
+        {
+            var blobClient = _containerClient.GetBlobClient(command.BlobName);
+            await blobClient.DeleteIfExistsAsync();
+        }
+    }
+}
