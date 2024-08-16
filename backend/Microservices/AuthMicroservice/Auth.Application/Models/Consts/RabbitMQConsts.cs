b2f6bb03a9fb4137572192e1b619a7c1eec13c8d
AnnaZalevskaya <123122231+AnnaZalevskaya@users.noreply.github.com>
2024-08-16 10:24:55 +0300

    changing-rabbitmq-configuration

--- backend/Microservices/AuthMicroservice/Auth.Application/Models/Consts/RabbitMQConsts.cs
+++ backend/Microservices/AuthMicroservice/Auth.Application/Models/Consts/RabbitMQConsts.cs
@@ -2,7 +2,7 @@
 {
     public class RabbitMQConsts
     {
-        public const string Host = "localhost";
+        public const string Uri = "amqps://mweuchuh:RgZ7P85Ml6YbuztOD8W7_g2UwPPi1p1g@goose.rmq2.cloudamqp.com/mweuchuh";
         public const string SendingQueue = "users_queue";
     }
 }
